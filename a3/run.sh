1f8273305bb6d18718b43112887586ce4708e7ac
Eugene Wang <eugene.wang@uwaterloo.ca>
2021-11-23 19:38:36 -0500

    A3 completed and push

--- a3/run.sh
+++ a3/run.sh
@@ -1,9 +1,7 @@
-rm -rf build
-mkdir build
-cd build
+rm -rf build && mkdir build && cd build
 cmake ../
 make install
-pwd
-cp ../ece650-a1.py .
+cd ./run/bin
+# cp ../ece650-a1.py .
 ./ece650-a3 -s 5 -n 4 -l 6 -c 21
 
